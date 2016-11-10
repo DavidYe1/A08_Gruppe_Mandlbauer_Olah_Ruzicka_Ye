39498af9c0a06e20bc2c9567c02e7d80b6d10f4e
David <david.ye@live.de>
2016-11-10 13:25:34 +0100

    Interfaces

---
+++ src/IFlaeche.java
@@ -0,0 +1,6 @@
+/**
+ * Created by @Autor David Ye on @Version 10.11.2016.
+ */
+public interface IFlaeche {
+    public double getFlaeche();
+}
---
+++ src/IZahlen.java
@@ -0,0 +1,12 @@
+/**
+ * Created by @Autor David Ye on @Version 10.11.2016.
+ */
+public interface IZahlen {
+    public double getSumme();
+
+    public double getMinimum();
+
+    public double getMaximum();
+
+    public void add(double value);
+}
