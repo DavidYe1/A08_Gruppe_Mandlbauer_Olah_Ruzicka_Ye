f35b4ff9331bf2cd6bec99c625208d2ae5e98eae
David <david.ye@live.de>
2016-11-11 17:38:39 +0100

    Zahlen

---
+++ src/Zahlen.java
@@ -0,0 +1,25 @@
+/**
+ * Created by @Autor DavidYe on @Version 11.11.2016.
+ */
+public class Zahlen implements IZahlen {
+
+    @Override
+    public double getSumme() {
+        return 0;
+    }
+
+    @Override
+    public double getMinimum() {
+        return 0;
+    }
+
+    @Override
+    public double getMaximum() {
+        return 0;
+    }
+
+    @Override
+    public void add(double value) {
+
+    }
+}
