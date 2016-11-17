153807e8e99a333a2936a2fc2adbd2708f25a01a
David <david.ye@live.de>
2016-11-17 12:41:30 +0100

    comments

--- src/Zahlen.java
+++ src/Zahlen.java
@@ -8,11 +8,18 @@ public class Zahlen implements IZahlen {
 
     public static LinkedList<Double> v = new LinkedList<Double>();
 
+    /**
+     * Konstruktor
+     */
     public Zahlen() {
         LinkedList<Double> v = Zahlen.v;
     }
 
-
+    /**
+     * addiert alle Zahlen von der einer Liste
+     *
+     * @return summe
+     */
     @Override
     public double getSumme() {
         double erg = 0;
@@ -20,13 +27,21 @@ public class Zahlen implements IZahlen {
             erg += sumItem;
         }
         return erg;
-    }
+    }/**/
 
+    /**
+     * findet die kleinste Zahl aus einer Liste
+     * @return min
+     */
     @Override
     public double getMinimum() {
         return Collections.min(v);
     }
 
+    /**
+     * findet die größte Zahl aus einer Liste
+     * @return max
+     */
     @Override
     public double getMaximum() {
         return Collections.max(v);
